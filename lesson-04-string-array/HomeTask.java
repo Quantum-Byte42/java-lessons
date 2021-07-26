745e6452efa39a0241df6b86cfd5584afbff9083
Quantum-Byte42 <nikitakosminskiy@gmail.com>
2021-07-27 00:06:22 +0300

    i got 5 errors

--- lesson-04-string-array/HomeTask.java
+++ lesson-04-string-array/HomeTask.java
@@ -24,13 +24,30 @@ class StringProcessor {
 	boolean isSentence() {
 		// TODO: return true if str starts from title case letter (use
 		// isTitleCase() method) and ends by '.', '!' or '?'.
-		return false;
+		char first = str.charAt(1);
+		char last = str.charAt(-1);
+		boolean gend = false;
+		if (last = '.' || last = '!' || last = '?'){
+		     gend = true;
+		}
+         if (isTitleCase(first) == true && gend == true){
+             return true;
+		 }
+           else{ 
+		     return false;
+		   }
 	}
 
 	String getFirstWord() {
 		// TODO: return the first word of the str. Use isSpace() method to find
 		// if character is a space.
 		String word = "";
+		char ch;
+		int i = 1;
+		while (isSpace(ch) == false){
+			ch = str.charAt(i);
+			word = word + ch;
+			i += 1;
 		return word;
 	}
 
@@ -38,13 +55,24 @@ class StringProcessor {
 		// TODO: return the last word of the str. Use isSpace() method to find
 		// if character is a space.
 		String word = "";
+		char ch;
+		int i = -1;
+		while (isSpace(ch) == false){
+			ch = str.charAt(i);
+			word = ch + word;
+			i -= 1;
 		return word;
 	}
 
 	boolean contains(String other) {
 		// TODO: return true if str contains the argument. Hint: you can check
 		// String.indexOf() method documentation
-		return false;
+		if (str.indexOf() != -1){
+			return true;
+		}
+		else{ 
+		  return false;
+		 }
 	}
 
 }
@@ -54,7 +82,12 @@ class ArrayGenerator {
 	int[] getPowers(int x, int count) {
 		// TODO: generate and return the array of powers of the x starting from
 		// 0. For example getPowers(2, 4) should return [ 1, 2, 4, 8 ]
-		int[] powers = {};
+		int[] powers = new int [count];
+		int i = 0;
+		powers[0] = 1;
+		for (i = 0; i<count; i+=1){
+			powers[i] = powers[i]*x;
+		}
 		return powers;
 	}
 
@@ -74,13 +107,28 @@ class ArrayProcessor {
 
 	int sum() {
 		// TODO: return the sum of the elements of the nums array
-		return 0;
+		int i = 0;
+		int sum = 0;
+		int lng = nums.length;
+		for (i = 0; i<lng; i+=1){
+			sum += nums[i];
+		}
+		return sum;
 	}
 
 	boolean[] even() {
 		// TODO: retun array of booleans each element contains true if
 		// corresponding element of the nums is even and false otherwise.
 		boolean[] even = {};
+		int i = 0;
+		for (i = 0; i<nums.length; i +=1){
+			if (nums[i] % 2 ==0){
+				even[i] = true;
+			}
+				else{ 
+				 even[i] = false;
+				}
+			}
 		return even;
 	}
 
